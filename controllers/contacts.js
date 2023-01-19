1e18c380ebe7fc40a53b4ecf7400f54ef5a34cbd
kleigh23 <rob22003@byui.edu>
2023-01-19 14:32:05 -0700

    did personal project w03

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -22,4 +22,57 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const result = await mongodb
+  .getDb()
+  .db('myDatabase')
+  .collection('contacts')
+  .insertOne(contact);
+  if (result.acknowledged) {
+    res.status(201).json(result);
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while creating the contact.');
+  }
+}
+
+const updateContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+  .getDb()
+  .db('myDatabase')
+  .collection('contacts')
+  .replaceOne({ _id: userId }, contact);
+  console.log(result);
+  if (result.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while updating the contact.');
+  }
+}
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db('myDatabase').collection('contacts').remove({ _id: userId }, true);
+  console.log(result);
+  if (result.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact};
