ed5b921d4bc0dc5b5d41438d6bb7f1b990f2551a
kleigh23 <rob22003@byui.edu>
2023-01-27 14:41:10 -0700

    got it to work locally w04

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
-const port = process.env.PORT || 3000;
+
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app
   .use(bodyParser.json())
@@ -11,8 +13,10 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use('/', require('./routes'));
+  .use('/', require('./routes'))
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+const port = process.env.PORT || 3000;
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
