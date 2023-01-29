9fb451cdba8be880e2badffff39226d38b47802b
kleigh23 <rob22003@byui.edu>
2023-01-28 17:23:15 -0700

    work on render

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: 'localhost:3000',
-  schemes: ['http'],
+  host: 'web-backend-2.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = 'swagger.json';
